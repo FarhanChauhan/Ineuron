0776decac4ec83b30f5a00b652b3fa023f755bc5
FarhanChauhan <farhanchauhan950@gmail.com>
2023-02-27 12:18:54 +0530

    first commit

---
+++ DemoApp/src/main/java/in/ineuron/main/Launch1.java
@@ -0,0 +1,16 @@
+package in.ineuron.main;
+
+public class Launch1 {
+
+	public static void main(String[] args) {
+		
+		int [] arr = {10,20,30,40,50,60,70};
+		System.out.println(arr.length);
+		
+		String name = "sachin";
+		int count =name.toUpperCase().length();
+		System.out.println(count);
+		
+	}
+
+}
