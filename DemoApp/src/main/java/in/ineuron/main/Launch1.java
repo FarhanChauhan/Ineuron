af973eb1676854388c6fab8048759581f6c9a5da
FarhanChauhan <farhanchauhan950@gmail.com>
2023-03-09 13:31:50 +0530

    farhan

--- DemoApp/src/main/java/in/ineuron/main/Launch1.java
+++ DemoApp/src/main/java/in/ineuron/main/Launch1.java
@@ -1,16 +1,97 @@
 package in.ineuron.main;
-
 public class Launch1 {
 
-	public static void main(String[] args) {
-		
-		int [] arr = {10,20,30,40,50,60,70};
-		System.out.println(arr.length);
-		
-		String name = "sachin";
-		int count =name.toUpperCase().length();
-		System.out.println(count);
-		
-	}
 
-}
+		public static void main(String[] args) {
+			int n= 7;
+			for (int i=0 ; i<n ; i++)
+			{
+				//F
+				for (int j=0 ; j<n ; j++)
+				{
+					if(i==0 || j==0 || i==(n-1)/2)  
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+				}
+				System.out.print("   ");
+				
+				//A
+				for (int j=0 ; j<n ; j++)
+				{
+					if(i==0&&j!=0&&j!=n-1 || j==0 && i!=0 || i==(n-1)/2 || j==(n-1)&&i>0)  
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+				}
+				System.out.print("   ");
+				
+				//R
+				for (int j=0 ; j<n ; j++)
+				{
+					if(i==0 || j==0 || i==(n-1)/2  || j==n-1&&i<(n-1)/2|| i==j&&i>(n-1)/2)  
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+				}
+				System.out.print("   ");
+				
+				//H
+				for (int j=0 ; j<n ; j++)
+				{
+					if(j==n-1 || j==0 || i==(n-1)/2 ||j==n-1&&i<(n-1)/2)  
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+				}
+				System.out.print("   ");
+				
+				//A
+				for (int j=0 ; j<n ; j++)
+				{
+					if(i==0&&j!=0&&j!=n-1 || j==0&&i>0 || i==(n-1)/2 || j==n-1&&i>0)  
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+				}
+				System.out.print("   ");
+				
+				//N
+				for (int j=0 ; j<n ; j++)
+				{
+					if( i==j  || j==0 || j==n-1)  
+					{
+						System.out.print("*");
+					}
+					else
+					{
+						System.out.print(" ");
+					}
+				}
+				
+				System.out.println();
+			}
+			
+		}
+
+	}
